5967be430cd12bee89dc7cb651ab23a587193a3c
user <user@DESKTOP-5AV7EV5>
2023-08-24 15:56:33 +0300

    Add project files.

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,92 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context) 
+        { 
+            _context = context;
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+
+            if (review.Count() <= 0)
+                return 0;
+            
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public bool DeletePokemon(Pokemon pokemon)
+        {
+            _context.Remove(pokemon);
+            return Save();
+        }
+
+        public bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+        {
+            var pokemonOwnerEntity = _context.Owners.Where(a => a.Id == ownerId).FirstOrDefault();
+            var category = _context.Categories.Where(a => a.Id == categoryId).FirstOrDefault();
+            var pokemonOwner = new PokemonOwner()
+            {
+                Owner = pokemonOwnerEntity,
+                Pokemon = pokemon,
+            };
+
+            _context.Add(pokemonOwner);
+
+            var pokemonCategory = new PokemonCategory()
+            {
+                Category = category,
+                Pokemon = pokemon,
+            };
+            _context.Add(pokemonCategory);
+            _context.Add(pokemon);
+
+            return Save();
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeid)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokeid);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdatePokemon(int ownerId, int CategoryId, Pokemon pokemon)
+        {
+            _context.Update(pokemon);
+            return Save();
+        }
+
+        //decimal IPokemonRepository.GetPokemonRating(int pokeId)
+        // {
+        //   throw new NotImplementedException();
+        //}
+    }
+}
